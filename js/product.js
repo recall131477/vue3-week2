bd2d3bfa8b2ddb8302781e24f840d18a6ac850fa
rousong <recall131477@gmail.com>
2022-01-19 19:27:17 +0800

    將html的js整合於js檔案、並修改函式架構與新增提示文字

---
+++ js/product.js
@@ -0,0 +1,60 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.1.4/vue.esm-browser.min.js';
+const app = createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'rousong',
+      products: [],
+      currentProduct: {},
+    }
+  },
+  methods: {
+    checkLogin() {
+      // 取出token
+      const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      axios.defaults.headers.common['Authorization'] = token;
+
+      const url = `${this.apiUrl}/api/user/check`;
+      axios.post(url)
+        .then((res) => {
+          this.getData();
+        })
+        .catch((err) => {
+          console.dir(err);
+          alert(err.data.message);
+          window.location = 'index.html';
+        })
+    },
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+      axios.get(url)
+        .then((res) => {
+          this.products = res.data.products;
+        })
+        .catch((err) => {
+          console.dir(err);
+        })
+    },
+    openProductDetail(item) {
+      this.currentProduct = item;
+    },
+    deleteProduct(item) {
+      const id = item.id;
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${id}`;
+      axios.delete(url)
+        .then((res) => {
+          const index = this.products.findIndex(obj => obj.id === item.id);
+          this.products.splice(index, 1);
+          alert('刪除成功');
+        })
+        .catch((err) => {
+          console.dir(err);
+          alert('刪除失敗');
+        })
+    }
+  },
+  created() {
+    this.checkLogin();
+  }
+});
+app.mount('#app');
